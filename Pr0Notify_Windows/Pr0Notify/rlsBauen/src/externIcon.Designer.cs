1e121d410a6631406b6ee66e8146466e4211a204
Marco.Silipo <m.silipo@web.de>
2015-04-02 16:00:48 +0200

    Initial Commit

---
+++ Pr0Notify_Windows/Pr0Notify/rlsBauen/src/externIcon.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Pr0Notify
+{
+    partial class externIcon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(externIcon));
+            this.SuspendLayout();
+            // 
+            // externIcon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(70, 40);
+            this.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "externIcon";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "externIcon";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.externIcon_Load);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.externIcon_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.externIcon_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.externIcon_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.externIcon_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Pr0Notify_Windows/Pr0Notify/rlsBauen/src/externIcon.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr0Notify
+{
+    public partial class externIcon : Form
+    {
+        public bool dragged;
+        public bool skip = true;
+        public Point lastPos;
+        public bool allowSizeChange;
+        public bool allowSizeChange_skip;
+        public externIcon()
+        {
+            allowSizeChange = false;
+            allowSizeChange_skip = false;
+            dragged = false;
+            skip = false;
+            InitializeComponent();
+            this.MouseWheel += externIcon_MouseWheel;
+        }
+
+        void externIcon_MouseWheel(object sender, MouseEventArgs e)
+        {
+           if(dragged)
+           {
+               allowSizeChange = true;
+               int newWidth = (int)(this.Size.Width + ((e.Delta > 0 ? 1 : -1) * 7));
+               int newHeight = (int)(this.Size.Height + ((e.Delta > 0 ? 1 : -1) * 4));
+
+               if (newWidth < 70)
+                   newWidth = 70;
+
+               if (newHeight < 40)
+                   newHeight = 40;
+
+               if (newWidth > 70 * 3)
+                   newWidth = 70 * 3;
+
+               if (newHeight > 40 * 3)
+                   newHeight = 40 * 3;
+
+
+               this.Size = new Size(newWidth, newHeight);
+               Pr0Notify.Properties.Settings.Default.externIcon_width = newWidth;
+               Pr0Notify.Properties.Settings.Default.externIcon_height = newHeight;
+               Pr0Notify.Properties.Settings.Default.Save();
+
+           }
+        }
+
+        private void externIcon_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragged = true;
+        }
+
+        private void externIcon_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragged = false;
+        }
+
+        private void externIcon_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragged && !skip)
+            {
+                if (lastPos == null)
+                    lastPos = e.Location;
+                this.Location = new Point(this.Location.X + e.X - lastPos.X, this.Location.Y + e.Y - lastPos.Y);
+                skip = true;
+                lastPos = e.Location;
+                return;
+            }
+            lastPos = e.Location;
+            skip = false;
+        }
+
+        private void externIcon_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(Pr0Notify.Properties.Settings.Default.externIcon_width, Pr0Notify.Properties.Settings.Default.externIcon_height);
+        }
+
+        private void externIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Program.doDoubleClick();
+        }
+    }
+}
